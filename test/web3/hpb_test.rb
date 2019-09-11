782f6c90e7e532707c05e0ea90e5986b8ff14522
Nicemanss <43435071+Nicemanss@users.noreply.github.com>
2019-09-11 22:04:18 +0200

    Update hpb_test.rb

--- test/web3/hpb_test.rb
+++ test/web3/hpb_test.rb
@@ -5,7 +5,13 @@ class Web3::HpbTest < Minitest::Test
      refute_nil ::Web3::Hpb::VERSION
   end
 
-  def test_it_does_something_useful
-    assert true
+  def test_that_it_has_hpb_version
+    web3 = Web3::Hpb::Rpc.new host: 'node.myhpbwallet.com',
+                              port: 443,
+                              connect_options: { open_timeout: 20, read_timeout: 120, use_ssl: true }
+    htp_version = web3.hpb.version
+    assert htp_version != nil
+    assert htp_version.is_a?(String)
+    assert htp_version == '100'
   end
 end

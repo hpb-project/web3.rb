397f1ce0ea57a00c4f4b5dcfe3b5e4c899e40a8a
Nicemanss <43435071+Nicemanss@users.noreply.github.com>
2019-09-11 16:50:25 +0200

    Update hpb_test.rb

--- test/web3/hpb_test.rb
+++ test/web3/hpb_test.rb
@@ -1,11 +1,11 @@
-require 'test_helper'
+require File.expand_path("../../test_helper", __FILE__)
 
 class Web3::HpbTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Web3::Hpb::VERSION
+     refute_nil ::Web3::Hpb::VERSION
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
   end
 end
